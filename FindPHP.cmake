529d61547eacb9cd43acd6d4ccceca202850279a
Paul Colby <git@colby.id.au>
2012-11-05 04:58:25 +0000

    Tweaked the generated variable names to be more consistent with http://cmake.org/gitweb?p=cmake.git;a=blob;f=Modules/readme.txt

--- FindPHP.cmake
+++ FindPHP.cmake
@@ -9,15 +9,18 @@
 # for php-config documentation.
 #
 # This code sets the following variables:
-#  PHP_CONFIG_DIR        = directory containing PHP configuration files
-#  PHP_CONFIG_EXECUTABLE = full path to the php-config binary
-#  PHP_EXECUTABLE        = full path to the php5 binary
-#  PHP_EXTENSION_DIR     = directory containing PHP extensions
-#  PHP_INCLUDE_DIR       = directory containing PHP extension headers
-#  PHP_INCLUDES          = include directives for PHP development
-#  PHP_VERNUM            = PHP version number eg 50303
-#  PHP_VERSION           = PHP version string eg 5.3.3-1ubuntu9.3
-#  PHPDEV_FOUND          = set to TRUE if all of the above has been found.
+#  PHP_CONFIG_DIR             = directory containing PHP configuration files
+#  PHP_CONFIG_EXECUTABLE      = full path to the php-config binary
+#  PHP_EXECUTABLE             = full path to the php binary
+#  PHP_EXTENSIONS_DIR         = directory containing PHP extensions
+#  PHP_EXTENSIONS_INCLUDE_DIR = directory containing PHP extension headers
+#  PHP_INCLUDE_DIRS           = include directives for PHP development
+#  PHP_VERSION_NUMBER         = PHP version number in PHP's "vernum" format eg 50303
+#  PHP_VERSION_MAJOR          = PHP major version number eg 5
+#  PHP_VERSION_MINOR          = PHP minor version number eg 3
+#  PHP_VERSION_PATCH          = PHP patch version number eg 3
+#  PHP_VERSION_STRING         = PHP version string eg 5.3.3-1ubuntu9.3
+#  PHP_FOUND                  = set to TRUE if all of the above has been found.
 #
 
 #=============================================================================
@@ -54,35 +57,60 @@ if (PHP_CONFIG_EXECUTABLE)
   execute_process(
     COMMAND
       ${PHP_CONFIG_EXECUTABLE} --extension-dir
-      OUTPUT_VARIABLE PHP_EXTENSION_DIR
+      OUTPUT_VARIABLE PHP_EXTENSIONS_DIR
       OUTPUT_STRIP_TRAILING_WHITESPACE
   )
 
   execute_process(
     COMMAND
       ${PHP_CONFIG_EXECUTABLE} --include-dir
-      OUTPUT_VARIABLE PHP_INCLUDE_DIR
+      OUTPUT_VARIABLE PHP_EXTENSIONS_INCLUDE_DIR
       OUTPUT_STRIP_TRAILING_WHITESPACE
   )
 
   execute_process(
     COMMAND
       ${PHP_CONFIG_EXECUTABLE} --includes
-      OUTPUT_VARIABLE PHP_INCLUDES
+      OUTPUT_VARIABLE PHP_INCLUDE_DIRS
       OUTPUT_STRIP_TRAILING_WHITESPACE
   )
 
   execute_process(
     COMMAND
       ${PHP_CONFIG_EXECUTABLE} --vernum
-      OUTPUT_VARIABLE PHP_VERNUM
+      OUTPUT_VARIABLE PHP_VERSION_NUMBER
       OUTPUT_STRIP_TRAILING_WHITESPACE
   )
 
   execute_process(
     COMMAND
+      ${PHP_CONFIG_EXECUTABLE} --vernum
+      OUTPUT_STRIP_TRAILING_WHITESPACE
+    COMMAND sed -ne "s/....$//p"
+      OUTPUT_VARIABLE PHP_VERSION_MAJOR
+  )
+
+  execute_process(
+    COMMAND
+      ${PHP_CONFIG_EXECUTABLE} --vernum
+      OUTPUT_STRIP_TRAILING_WHITESPACE
+    COMMAND sed -ne "s/..$//p"
+    COMMAND sed -ne "s/^.0\\?//p"
+      OUTPUT_VARIABLE PHP_VERSION_MINOR
+  )
+
+  execute_process(
+    COMMAND
+      ${PHP_CONFIG_EXECUTABLE} --vernum
+      OUTPUT_STRIP_TRAILING_WHITESPACE
+    COMMAND sed -ne "s/^...0\	\?//p"
+      OUTPUT_VARIABLE PHP_VERSION_PATCH
+  )
+
+  execute_process(
+    COMMAND
       ${PHP_CONFIG_EXECUTABLE} --version
-      OUTPUT_VARIABLE PHP_VERSION
+      OUTPUT_VARIABLE PHP_VERSION_STRING
       OUTPUT_STRIP_TRAILING_WHITESPACE
   )
 endif (PHP_CONFIG_EXECUTABLE)
@@ -91,33 +119,42 @@ MARK_AS_ADVANCED(
   PHP_CONFIG_DIR
   PHP_CONFIG_EXECUTABLE
   PHP_EXECUTABLE
-  PHP_EXTENSION_DIR
-  PHP_INCLUDE_DIR
-  PHP_INCLUDES
-  PHP_VERNUM
-  PHP_VERSION
+  PHP_EXTENSIONS_DIR
+  PHP_EXTENSIONS_INCLUDE_DIR
+  PHP_INCLUDE_DIRS
+  PHP_VERSION_MAJOR
+  PHP_VERSION_MINOR
+  PHP_VERSION_PATCH
+  PHP_VERSION_STRING
 )
 
 INCLUDE(FindPackageHandleStandardArgs)
 FIND_PACKAGE_HANDLE_STANDARD_ARGS(
-  phpdev
+  php
   DEFAULT_MSG
-  PHP_VERSION
+  PHP_VERSION_STRING
   PHP_CONFIG_DIR
   PHP_CONFIG_EXECUTABLE
   PHP_EXECUTABLE
-  PHP_EXTENSION_DIR
-  PHP_INCLUDE_DIR
-  PHP_INCLUDES
-  PHP_VERNUM
+  PHP_EXTENSIONS_DIR
+  PHP_EXTENSIONS_INCLUDE_DIR
+  PHP_INCLUDE_DIRS
+  PHP_VERSION_NUMBER
+  PHP_VERSION_MAJOR
+  PHP_VERSION_MINOR
+  PHP_VERSION_PATCH
+  PHP_VERSION_STRING
 )
 
-#MESSAGE("PHP_CONFIG_DIR        = ${PHP_CONFIG_DIR}")
-#MESSAGE("PHP_CONFIG_EXECUTABLE = ${PHP_CONFIG_EXECUTABLE}")
-#MESSAGE("PHP_EXECUTABLE        = ${PHP_EXECUTABLE}")
-#MESSAGE("PHP_EXTENSION_DIR     = ${PHP_EXTENSION_DIR}")
-#MESSAGE("PHP_INCLUDE_DIR       = ${PHP_INCLUDE_DIR}")
-#MESSAGE("PHP_INCLUDES          = ${PHP_INCLUDES}")
-#MESSAGE("PHP_VERNUM            = ${PHP_VERNUM}")
-#MESSAGE("PHP_VERSION           = ${PHP_VERSION}")
-#MESSAGE("PHPDEV_FOUND          = ${PHPDEV_FOUND}")
+MESSAGE("PHP_CONFIG_DIR             = ${PHP_CONFIG_DIR}")
+MESSAGE("PHP_CONFIG_EXECUTABLE      = ${PHP_CONFIG_EXECUTABLE}")
+MESSAGE("PHP_EXECUTABLE             = ${PHP_EXECUTABLE}")
+MESSAGE("PHP_EXTENSIONS_DIR         = ${PHP_EXTENSIONS_DIR}")
+MESSAGE("PHP_EXTENSIONS_INCLUDE_DIR = ${PHP_EXTENSIONS_INCLUDE_DIR}")
+MESSAGE("PHP_INCLUDE_DIRS           = ${PHP_INCLUDE_DIRS}")
+MESSAGE("PHP_VERSION_NUMBER         = ${PHP_VERSION_NUMBER}")
+MESSAGE("PHP_VERSION_MAJOR          = ${PHP_VERSION_MAJOR}")
+MESSAGE("PHP_VERSION_MINOR          = ${PHP_VERSION_MINOR}")
+MESSAGE("PHP_VERSION_PATCH          = ${PHP_VERSION_PATCH}")
+MESSAGE("PHP_VERSION_STRING         = ${PHP_VERSION_STRING}")
+MESSAGE("PHP_FOUND                  = ${PHP_FOUND}")
