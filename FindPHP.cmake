a15ac4418311bdce78705ddf108d22f01d4926b1
Jerry Jacobs <jerry.jacobs@dualinventive.com>
2016-01-22 10:28:13 +0100

    Update and add a C++ example

--- FindPHP.cmake
+++ FindPHP.cmake
@@ -1,6 +1,6 @@
 # - Find PHP
 # This module finds if PHP is installed and determines where the include files
-# and libraries are. 
+# and libraries are.
 #
 # Note, unlike the FindPHP4 module, this module uses the php-config script to
 # determine information about the installed PHP configuration.  For Linux
@@ -15,15 +15,18 @@
 #  PHP_EXTENSIONS_DIR         = directory containing PHP extensions
 #  PHP_EXTENSIONS_INCLUDE_DIR = directory containing PHP extension headers
 #  PHP_INCLUDE_DIRS           = include directives for PHP development
+#  PHP_LIBS                   = libraries necessary
 #  PHP_VERSION_NUMBER         = PHP version number in PHP's "vernum" format eg 50303
 #  PHP_VERSION_MAJOR          = PHP major version number eg 5
 #  PHP_VERSION_MINOR          = PHP minor version number eg 3
 #  PHP_VERSION_PATCH          = PHP patch version number eg 3
 #  PHP_VERSION_STRING         = PHP version string eg 5.3.3-1ubuntu9.3
+#  PHP_VERSION_API            = PHP version api string eg 20131226
 #  PHP_FOUND                  = set to TRUE if all of the above has been found.
 #
 
 #=============================================================================
+# Copyright 2016 Jerry Jacobs
 # Copyright 2011-2012 Paul Colby
 #
 # Distributed under the OSI-approved BSD License (the "License");
@@ -42,7 +45,7 @@ if (PHP_CONFIG_EXECUTABLE)
   execute_process(
     COMMAND
       ${PHP_CONFIG_EXECUTABLE} --configure-options
-    COMMAND sed -ne "s/^.*--with-config-file-scan-dir=\\([^ ]*\\).*/\\1/p"
+    #COMMAND sed -ne "s/^.*--with-config-file-scan-dir=\\([^ ]*\\).*/\\1/p"
       OUTPUT_VARIABLE PHP_CONFIG_DIR
       OUTPUT_STRIP_TRAILING_WHITESPACE
   )
@@ -71,9 +74,12 @@ if (PHP_CONFIG_EXECUTABLE)
   execute_process(
     COMMAND
       ${PHP_CONFIG_EXECUTABLE} --includes
-      OUTPUT_VARIABLE PHP_INCLUDE_DIRS
+      OUTPUT_VARIABLE _PHP_INCLUDE_DIRS
       OUTPUT_STRIP_TRAILING_WHITESPACE
   )
+
+  # Create CMAKE list
+  string(REPLACE "-I" ";" PHP_INCLUDE_DIRS ${_PHP_INCLUDE_DIRS})
 
   execute_process(
     COMMAND
@@ -103,7 +109,7 @@ if (PHP_CONFIG_EXECUTABLE)
     COMMAND
       ${PHP_CONFIG_EXECUTABLE} --vernum
       OUTPUT_STRIP_TRAILING_WHITESPACE
-    COMMAND sed -ne "s/^...0\	\?//p"
+#    COMMAND sed -ne "s/^...0\	\?//p"
       OUTPUT_VARIABLE PHP_VERSION_PATCH
   )
 
@@ -111,6 +117,13 @@ if (PHP_CONFIG_EXECUTABLE)
     COMMAND
       ${PHP_CONFIG_EXECUTABLE} --version
       OUTPUT_VARIABLE PHP_VERSION_STRING
+      OUTPUT_STRIP_TRAILING_WHITESPACE
+  )
+
+  execute_process(
+    COMMAND
+      ${PHP_CONFIG_EXECUTABLE} --libs
+      OUTPUT_VARIABLE PHP_LIBS
       OUTPUT_STRIP_TRAILING_WHITESPACE
   )
 endif (PHP_CONFIG_EXECUTABLE)
@@ -122,6 +135,7 @@ MARK_AS_ADVANCED(
   PHP_EXTENSIONS_DIR
   PHP_EXTENSIONS_INCLUDE_DIR
   PHP_INCLUDE_DIRS
+  PHP_LIBS
   PHP_VERSION_MAJOR
   PHP_VERSION_MINOR
   PHP_VERSION_PATCH
@@ -139,6 +153,8 @@ FIND_PACKAGE_HANDLE_STANDARD_ARGS(
   PHP_EXTENSIONS_DIR
   PHP_EXTENSIONS_INCLUDE_DIR
   PHP_INCLUDE_DIRS
+  PHP_LIBS
+  PHP_VERSION_NUMBER
   PHP_VERSION_NUMBER
   PHP_VERSION_MAJOR
   PHP_VERSION_MINOR
@@ -147,15 +163,16 @@ FIND_PACKAGE_HANDLE_STANDARD_ARGS(
 )
 
 # Some handy dev output. Is there a way to enable these in some debug mode?
-#MESSAGE("PHP_CONFIG_DIR             = ${PHP_CONFIG_DIR}")
-#MESSAGE("PHP_CONFIG_EXECUTABLE      = ${PHP_CONFIG_EXECUTABLE}")
-#MESSAGE("PHP_EXECUTABLE             = ${PHP_EXECUTABLE}")
-#MESSAGE("PHP_EXTENSIONS_DIR         = ${PHP_EXTENSIONS_DIR}")
-#MESSAGE("PHP_EXTENSIONS_INCLUDE_DIR = ${PHP_EXTENSIONS_INCLUDE_DIR}")
-#MESSAGE("PHP_INCLUDE_DIRS           = ${PHP_INCLUDE_DIRS}")
-#MESSAGE("PHP_VERSION_NUMBER         = ${PHP_VERSION_NUMBER}")
-#MESSAGE("PHP_VERSION_MAJOR          = ${PHP_VERSION_MAJOR}")
-#MESSAGE("PHP_VERSION_MINOR          = ${PHP_VERSION_MINOR}")
-#MESSAGE("PHP_VERSION_PATCH          = ${PHP_VERSION_PATCH}")
-#MESSAGE("PHP_VERSION_STRING         = ${PHP_VERSION_STRING}")
-#MESSAGE("PHP_FOUND                  = ${PHP_FOUND}")
+MESSAGE("PHP_CONFIG_DIR             = ${PHP_CONFIG_DIR}")
+MESSAGE("PHP_CONFIG_EXECUTABLE      = ${PHP_CONFIG_EXECUTABLE}")
+MESSAGE("PHP_EXECUTABLE             = ${PHP_EXECUTABLE}")
+MESSAGE("PHP_EXTENSIONS_DIR         = ${PHP_EXTENSIONS_DIR}")
+MESSAGE("PHP_EXTENSIONS_INCLUDE_DIR = ${PHP_EXTENSIONS_INCLUDE_DIR}")
+MESSAGE("PHP_INCLUDE_DIRS           = ${PHP_INCLUDE_DIRS}")
+MESSAGE("PHP_LIBS                   = ${PHP_LIBS}")
+MESSAGE("PHP_VERSION_NUMBER         = ${PHP_VERSION_NUMBER}")
+MESSAGE("PHP_VERSION_MAJOR          = ${PHP_VERSION_MAJOR}")
+MESSAGE("PHP_VERSION_MINOR          = ${PHP_VERSION_MINOR}")
+MESSAGE("PHP_VERSION_PATCH          = ${PHP_VERSION_PATCH}")
+MESSAGE("PHP_VERSION_STRING         = ${PHP_VERSION_STRING}")
+MESSAGE("PHP_FOUND                  = ${PHP_FOUND}")
